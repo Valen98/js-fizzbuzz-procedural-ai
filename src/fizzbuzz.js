60eb89f84f9e5459cc00c83dab4a46c3f0422c50
Valen98 <leo.wahlandtcs@gmail.com>
2024-10-24 09:41:33 +0200

    Solved fizzbuzz using Copilot

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -3,7 +3,19 @@ const answer = []
 // Write your code below this line
 
 
+// Solve this fizzbuzz challenge
 
+for (let i = 1; i <= 15; i++) {
+    if (i % 15 === 0) {
+        answer.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+        answer.push('Fizz');
+    } else if (i % 5 === 0) {
+        answer.push('Buzz');
+    } else {
+        answer.push(i);
+    }
+}
 
 
 // Don't touch the code below this line, we'll cover it later
